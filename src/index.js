a80244e070c3c4cf4cfcbca5197ff566ebeee5f3
grinya0101 <grinya1994015@gmail.com>
2023-03-02 07:27:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,43 +1,56 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const formEL = document.querySelector('.search-form');
 const btnEl = document.querySelector('.load-more');
 const divEl = document.querySelector('.gallery');
-console.log(divEl);
+console.log(btnEl);
 
 const API_KEY = '29209271-716f3ea82b952e36eef48fa19';
 
 formEL.addEventListener('submit', onSearch);
-btnEl.addEventListener('click', dada);
+btnEl.addEventListener('click', feachLoadMoreMurkup);
 
 let searchQuery = '';
 let page = 1;
 
 function onSearch(e) {
   e.preventDefault();
-  clearHitsContainer();
+
   searchQuery = e.currentTarget.elements.searchQuery.value;
-  fatchHits().then(onHitsMarkup);
+  // fetchHits().then(onHitsMarkup);
+  fetchHits().then(hits => {
+    clearHitsContainer();
+    onHitsMarkup(hits);
+  });
 }
 
-function fatchHits() {
+function fetchHits() {
   return fetch(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=3&page=1`
+    `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=1`
   )
     .then(r => r.json())
     .then(data => {
       page = 1;
-
+      onHiddenLoadMore()
+      
       return data.hits;
     });
 }
 
-function dada() {
+function onHiddenLoadMore() {
+  btnEl.hidden = false
+}
+
+
+function feachLoadMoreMurkup() {
   onLoadMore().then(onHitsMarkup);
 }
 
 function onLoadMore() {
   page += 1;
   return fetch(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=3&page=${page}`
+    `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${page}`
   )
     .then(r => r.json())
     .then(data => {
@@ -58,33 +71,47 @@ function onHitsMarkup(hits) {
         downloads,
       }) => {
         return `
+
     <div class="photo-card">
+    <a href="${largeImageURL}">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
   <div class="info">
     <p class="info-item">
+        likes
       <b>${likes}</b>
     </p>
     <p class="info-item">
+        views
       <b>${views}</b>
     </p>
     <p class="info-item">
+        comments
       <b>${comments}</b>
     </p>
     <p class="info-item">
+       downloads
       <b>${downloads}</b>
     </p>
   </div>
 </div>
+
 `;
       }
     )
 
     .join('');
 
-  divEl.insertAdjacentHTML('beforeend', result);
+    createMarkup(result)
+    var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 }
 
+function createMarkup(result) {
+  divEl.insertAdjacentHTML('beforeend', result);
+}
 
 function clearHitsContainer() {
   divEl.innerHTML = '';
 }
+
+
