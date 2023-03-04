95b321dc849e6c8bb466b8a045386d0ef94f6dcd
grinya0101 <grinya1994015@gmail.com>
2023-03-04 06:59:21 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
+import onHitsMarkup from './onHitsMarkup'
+import onLoadMore from './onLoadMore'
+
 
 const formEL = document.querySelector('.search-form');
 const btnEl = document.querySelector('.load-more');
 const divEl = document.querySelector('.gallery');
 console.log(btnEl);
 
-const API_KEY = '29209271-716f3ea82b952e36eef48fa19';
+export const API_KEY = '29209271-716f3ea82b952e36eef48fa19';
 
 formEL.addEventListener('submit', onSearch);
 btnEl.addEventListener('click', feachLoadMoreMurkup);
 
-let searchQuery = '';
-let page = 1;
+export let searchQuery = ''; 
+export let page = 1;
 
 function onSearch(e) {
   e.preventDefault();
@@ -24,17 +27,6 @@ function onSearch(e) {
   }
 
   clearHitsAndUpdate();
-}
-
-function clearHitsAndUpdate() {
-  try {
-    fetchHits().then(hits => {
-      clearHitsContainer();
-      onHitsMarkup(hits);
-    });
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 async function fetchHits() {
@@ -49,18 +41,32 @@ async function fetchHits() {
       'Sorry, there are no images matching your search query. Please try again'
     );
   }
-  page = 1;
+     page = 1;
 
   if (newHits.total > 1) {
-    onHiddenLoadMore();
+      onHiddenLoadMore()
   }
 
   return newHits.hits;
 }
 
-function onHiddenLoadMore() {
-  btnEl.hidden = false;
+
+
+function clearHitsAndUpdate() {
+  try {
+    fetchHits().then(hits => {
+      clearHitsContainer();
+      onHitsMarkup(hits);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+
+export default function onHiddenLoadMore() {
+  btnEl.hidden = false;
+}     
 
 function feachLoadMoreMurkup() {
   try {
@@ -70,78 +76,8 @@ function feachLoadMoreMurkup() {
   }
 }
 
-async function onLoadMore() {
-  page += 1;
-  const respons = await fetch(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=20&page=${page}`
-  );
 
-  const newHits = await respons.json();
-
-  return newHits.hits;
-}
-
-// Пример с then
-
-// function onLoadMore() {
-//   page += 1;
-//   return fetch(
-//     `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=20&page=${page}`
-//   )
-//     .then(r => r.json())
-//     .then(data => {
-//       return data.hits;
-//     });
-// }
-
-
-function onHitsMarkup(hits) {
-  const result = hits
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-
-    <div class="photo-card">
-    <a href="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  </a>
-  <div class="info">
-    <p class="info-item">
-    <b>Likes</b>
-    ${likes}
-    </p>
-    <p class="info-item">
-    <b>Views</b>
-                ${views}
-    </p>
-    <p class="info-item">
-    <b>Comments</b>
-    ${comments}
-    </p>
-    <p class="info-item">
-    <b>Downloads</b>
-                ${downloads}
-    </p>
-  </div>
-</div>
-
-`;}).join('');
-
-  createMarkup(result);
-  new SimpleLightbox('.gallery a', {
-   
-  });
-}
-
-function createMarkup(result) {
+export default function createMarkup(result) {
   divEl.insertAdjacentHTML('beforeend', result);
 }
 
